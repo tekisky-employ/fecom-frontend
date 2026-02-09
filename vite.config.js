5448eda7e5d34ff54a1f415f45c572183d440ca7
Gulam Kamran <gulamkamran.123@gmail.com>
2026-02-09 18:19:43 +0100

    edit vite

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,30 @@
-// import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   // server: {
-//   //   proxy: {
-//   //     "/user": {
-//   //       target: "http://localhost:5000",
-//   //       changeOrigin: true,
-//   //     },
-//   //     "/api": {
-//   //       target: "http://localhost:5000",
-//   //       changeOrigin: true,
-//   //     },
-//   //   },
-//   // },
-// });
-
 import { defineConfig } from "vite";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/user": {
+  //       target: "http://localhost:5000",
+  //       changeOrigin: true,
+  //     },
+  //     "/api": {
+  //       target: "http://localhost:5000",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
+
+// import { defineConfig } from "vite";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+// });
